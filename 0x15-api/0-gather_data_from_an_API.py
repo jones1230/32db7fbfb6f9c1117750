58816a99aed7c49d369bb15f241e3be35a21e4bf
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-31 20:58:23 +0000

    changed code in task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,26 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
-    todos = requests.get(url + 'todos', params={"userId": sys.argv[1]}).json()
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <employee_id>")
+        sys.exit(1)
 
-    completed = [todo for todo in todos if todo.get("completed") is True]
+    employee_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    # Fetch user data
+    user = requests.get(url + 'users/{}'.format(employee_id)).json()
+    # Fetch todo list data for the user
+    todos = requests.get(url + 'todos',
+                         params={"userId": employee_id}).json()
+
+    # List of completed tasks
+    completed = [todo for todo in todos if todo.get("completed")]
+
+    # Output the required information
     print('Employee {} is done with tasks({}/{}):'.format(
         user.get("name"), len(completed), len(todos)))
-    [print('\t {}'.format(c.get("title"))) for c in completed]
+
+    # Output each completed task
+    for task in completed:
+        print('\t {}'.format(task.get("title")))
