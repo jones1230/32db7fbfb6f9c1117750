05b232b2061fc5b817b86b604be323950901a5ac
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-08-13 20:50:11 +0000

    ...

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""Module for 0-subs.py"""
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
+    """Returns numbers od subs after reddit api query"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
+
     if sub_info.status_code >= 300:
         return 0
 
