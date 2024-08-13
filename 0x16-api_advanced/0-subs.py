082a80a91002e885213fa7309c6ea681856d340a
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-08-13 20:42:59 +0000

    remove pycodestyle errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """Function returns the number of subscribers for a given valid subreddit"""
 
+
 def number_of_subscribers(subreddit):
     """Returns numbers od subs after reddit api query"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                            headers={"User-agent": "My-User-Agent"}, allow_redirects=False)
-    
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-agent": "My-User-Agent"},
+                            allow_redirects=False)
+
     if sub_info.status_code >= 300:
         return 0
-    
+
     return sub_info.json().get("data").get("subscribers")
